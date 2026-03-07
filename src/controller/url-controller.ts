cafbe0b21242ac3269008c799b90943e0ebd7d23
Kleber Araujo <klbr16@outlook.com>
2026-03-07 17:01:02 -0300

    refactor: update createShortUrl typing and response structure

--- src/controller/url-controller.ts
+++ src/controller/url-controller.ts
@@ -6,11 +6,7 @@ export class UrlController {
 
     createShortUrl = async(req: Request<{}, {}, { url: string }>, res: Response) => {
         const { url } = req.body;
-        try {
-            const shortUrl = await this.urlService.generateShortUrl({ url });
-            res.status(201).json({ shortUrl: `http://localhost:3000/${shortUrl}` });
-        } catch (error) {
-            res.status(400).json({ error:"Failed to create short URL"});
-        }
+        const shortUrl = await this.urlService.generateShortUrl({ url });
+        res.status(201).json({ shortUrl: `http://localhost:3000/${shortUrl}` });
     }
  }
