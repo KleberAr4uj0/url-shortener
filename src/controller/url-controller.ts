5f948e4c06c5a0d81452d0110339b7ceb44c205f
Kleber Araujo <klbr16@outlook.com>
2026-03-07 15:57:34 -0300

    feat: add createShortUrl endpoint logic

--- src/controller/url-controller.ts
+++ src/controller/url-controller.ts
@@ -1,5 +1,16 @@
 import { UrlService } from "../service/url-service";
+import { Request, Response } from "express";
 
 export class UrlController {
     constructor(private urlService: UrlService) { }
+
+    createShortUrl = async(req: Request<{}, {}, { url: string }>, res: Response) => {
+        const { url } = req.body;
+        try {
+            const shortUrl = await this.urlService.generateShortUrl({ url });
+            res.status(201).json({ shortUrl: `http://localhost:3000/${shortUrl}` });
+        } catch (error) {
+            res.status(400).json({ error:"Failed to create short URL"});
+        }
+    }
  }
