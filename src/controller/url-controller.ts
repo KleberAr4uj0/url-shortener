14a4fe9a91e2244bb6733eefb78f191e3e4c927d
Kleber Araujo <klbr16@outlook.com>
2026-03-07 17:25:22 -0300

    feat: add redirectOriginalUrl endpoint

--- src/controller/url-controller.ts
+++ src/controller/url-controller.ts
@@ -9,4 +9,10 @@ export class UrlController {
         const shortUrl = await this.urlService.generateShortUrl({ url });
         res.status(201).json({ shortUrl: `http://localhost:3000/${shortUrl}` });
     }
+
+    redirectOriginalUrl = async (req: Request<{ code: string }>, res: Response) => {
+        const { code } = req.params;
+        const originalUrl = await this.urlService.getOriginalUrl({ short_url: code});
+        res.redirect(originalUrl);
+    }
  }
